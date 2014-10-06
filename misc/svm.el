a36d6492cfb8b16b56c281930d22de1611dc5dee
Steve Kemp <steve@steve.org.uk>
2014-10-06 23:16:39 +0100

    Added emacs mode.

---
+++ misc/svm.el
@@ -0,0 +1,21 @@
+;;
+;; Simple Emacs mode for the input files.
+;;
+;; I load this via:
+;;
+;;     (require 'svm)
+;;     (add-to-list 'auto-mode-alist '("\\.in\\'" . svm-mode))
+;;
+
+(setq svm-keywords
+ '(("^\s*add\\|^\s*sub\\|^\s*mul\\|^\s*div\\|^\s*inc\\|^\s*dec\\|^\s*system\\|^\s*concat\\|^\s*string2int\\|^\s*int2string\\|^\s*cmp\\|^\s*load\\|^\s*print\\|^\s*store\\|^\s*nop\\|^\s*exit" . font-lock-function-name-face)
+   ("^\s*goto\\|^\s*jmpnz\\|^\s*jmpz\\|^:[-_A-Za-z0-9]+" . font-lock-warning-face)
+  )
+)
+
+(define-derived-mode svm-mode fundamental-mode
+  (setq font-lock-defaults '(svm-keywords))
+  (setq mode-name "svm"))
+
+(provide 'svm)
+
