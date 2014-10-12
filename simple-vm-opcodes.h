c192911fb2eca16e73a2336db1063131cb1d46ae
Steve Kemp <steve@steve.org.uk>
2014-10-12 11:04:23 +0100

    Don't use "#define" for opcodes.
    We now use an enum, which gives us simpler ability to add
    new opcodes.

--- simple-vm-opcodes.h
+++ simple-vm-opcodes.h
@@ -22,100 +22,99 @@
 #define SIMPLE_VM_OPCODES_H 1
 
 
-
-/***
- * Our "opcodes".
- */
-#define OPCODE_EXIT   0x00
-
-
 /**
- * Integer things.
+ * This ENUM stores the values of our opcodes.
+ *
+ * This is less painful to update than having a series of #define instructions,
+ * and ensure that we have unique and incrementing values for the various
+ * opcodes.
+ *
+ * Opcodes are configured in bunches of 16, with instructions broadly
+ * grouped together by class/type.
+ *
  */
-#define INT_STORE     0x01
-#define INT_PRINT     0x02
-#define INT_TOSTRING  0x03
-#define INT_RANDOM    0x04
+enum opcode_values {
 
-/**
- * Jump things.
- */
-#define JUMP_TO       0x10
-#define JUMP_Z        0x11
-#define JUMP_NZ       0x12
+    /**
+     * Early opcodes.
+     */
+    EXIT = 0,
+    INT_STORE,
+    INT_PRINT,
+    INT_TOSTRING,
+    INT_RANDOM,
+
+    /**
+     * Jump operations.
+     */
+    JUMP_TO = 0x10,
+    JUMP_Z,
+    JUMP_NZ,
+
+    /**
+     * Math operations.
+     */
+    XOR = 0x20,
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    INC,
+    DEC,
+    AND,
+    OR,
+
+    /**
+     * String operations.
+     */
+    STRING_STORE = 0x30,
+    STRING_PRINT,
+    STRING_CONCAT,
+    STRING_SYSTEM,
+    STRING_TOINT,
+
+    /**
+     * Comparison operations.
+     */
+    CMP_REG = 0x40,
+    CMP_IMMEDIATE,
+    CMP_STRING,
+
+    /**
+     * Misc.
+     */
+    NOP = 0x50,
+
+    /**
+     * PEEK/POKE operations.
+     */
+    LOAD_FROM_RAM = 0x60,
+    STORE_IN_RAM,
+
+    /**
+     * Stack operations.
+     */
+    STACK_PUSH = 0x70,
+    STACK_POP,
+    STACK_RET,
+    STACK_CALL
+};
 
 
-/**
- * Math things.
- */
-#define XOR_OP 0x20
-#define ADD_OP 0x21
-#define SUB_OP 0x22
-#define MUL_OP 0x23
-#define DIV_OP 0x24
-#define INC_OP 0x25
-#define DEC_OP 0x26
-#define AND_OP 0x27
-#define OR_OP  0x28
 
-/**
- * String things.
- */
-#define STRING_STORE   0x30
-#define STRING_PRINT   0x31
-#define STRING_CONCAT  0x32
-#define STRING_SYSTEM  0x33
-#define STRING_TOINT   0x34
-
-
-/**
- * Comparisons
- */
-#define CMP_REG       0x40
-#define CMP_IMMEDIATE 0x41
-#define CMP_STRING    0x42
-
-
-/**
- * Misc
- */
-#define NOP_OP  0x50
-
-
-/**
- * RAM things.
- */
-#define LOAD_FROM_RAM 0x60
-#define STORE_IN_RAM  0x61
-
-
-/**
- * Stack things.
- */
-#define STACK_PUSH 0x70
-#define STACK_POP  0x71
-#define STACK_RET  0x72
-#define STACK_CALL 0x73
-
-
-
+/* 0x00 - 0x0F */
 _Bool op_exit(struct svm *in);
-
 _Bool op_int_store(struct svm *in);
 _Bool op_int_print(struct svm *in);
 _Bool op_int_tostring(struct svm *in);
 _Bool op_int_random(struct svm *in);
 
-_Bool op_string_store(struct svm *in);
-_Bool op_string_print(struct svm *in);
-_Bool op_string_concat(struct svm *in);
-_Bool op_string_system(struct svm *in);
-_Bool op_string_toint(struct svm *in);
-
+/* 0x10 - 0x1F */
 _Bool op_jump_to(struct svm *in);
 _Bool op_jump_z(struct svm *in);
 _Bool op_jump_nz(struct svm *in);
 
+/* 0x20 - 0x2F */
 _Bool op_xor(struct svm *in);
 _Bool op_or(struct svm *in);
 _Bool op_add(struct svm *in);
@@ -126,21 +125,30 @@ _Bool op_div(struct svm *in);
 _Bool op_inc(struct svm *in);
 _Bool op_dec(struct svm *in);
 
+/* 0x30 - 0x3F */
+_Bool op_string_store(struct svm *in);
+_Bool op_string_print(struct svm *in);
+_Bool op_string_concat(struct svm *in);
+_Bool op_string_system(struct svm *in);
+_Bool op_string_toint(struct svm *in);
+
+/* 0x40 - 0x4F */
 _Bool op_cmp_reg(struct svm *in);
 _Bool op_cmp_immediate(struct svm *in);
 _Bool op_cmp_string(struct svm *in);
 
+/* 0x50 - 0x5F */
 _Bool op_nop(struct svm *in);
 
+/* 0x60 - 0x6F */
 _Bool op_load_from_ram(struct svm *in);
 _Bool op_store_in_ram(struct svm *in);
 
+/* 0x70 - 0x7F */
 _Bool op_stack_push(struct svm *in);
 _Bool op_stack_pop(struct svm *in);
 _Bool op_stack_ret(struct svm *in);
 _Bool op_stack_call(struct svm *in);
-
-
 
 
 
