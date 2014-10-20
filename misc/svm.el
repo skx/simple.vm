996d1f4ff00ab743d976c55de70c515a9deff0fc
Steve Kemp <steve@steve.org.uk>
2014-10-20 19:18:46 +0100

    Highlight is_string + is_integer too.  D'oh.
    This updates #12.

--- misc/svm.el
+++ misc/svm.el
@@ -8,7 +8,7 @@
 ;;
 
 (setq svm-keywords
- '(("^\s*add\\|^\s*DB\\|^\s*DATA\\|^\s*sub\\|^\s*store\\|^\s*mul\\|^\s*ret\\|^\s*div\\|^\s*inc\\|^\s*dec\\|^\s*system\\|^\s*concat\\|^\s*string2int\\|^\s*int2string\\|^\s*cmp\\|^\s*load\\|^\s*print_int\\|^\s*print_str\\|^\s*push\\|^\s*pop\\|^\s*peek\\|^\s*poke\\|^\s*memcpy\\|^\s*nop\\|^\s*exit" . font-lock-function-name-face)
+ '(("^\s*add\\|^\s*DB\\|^\s*DATA\\|^\s*sub\\|^\s*store\\|^\s*mul\\|^\s*ret\\|^\s*div\\|^\s*inc\\|^\s*dec\\|^\s*system\\|^\s*concat\\|^\s*string2int\\|^\s*int2string\\|^\s*cmp\\|^\s*load\\|^\s*print_int\\|^\s*print_str\\|^\s*push\\|^\s*pop\\|^\s*peek\\|^\s*poke\\|^\s*is_string\\|^\s*is_integer\\|^\s*memcpy\\|^\s*nop\\|^\s*exit" . font-lock-function-name-face)
    ("^\s*goto\\|^\s*call\\|^\s*jmpnz\\|^\s*jmpz\\|^:[-_A-Za-z0-9]+" . font-lock-warning-face)
   )
 )
